f8527d51a749a0f29342781014f4f44532334677
Yassine Azzout <92bondstreet@gmail.com>
2013-06-11 16:00:43 +0200

    toAscii and trim_text functions

---
+++ swisscode.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * SwissCode aka PHP MacGyver
+ *
+ * Swiss Army Knife but for php coding
+ * Some (basic) utilities functions 
+ *
+ *	Copyright (c) 2013 - 92 Bond Street, Yassine Azzout
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in
+ *	all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package SwissCode
+ * @version 1.0
+ * @copyright 2013 - 92 Bond Street, Yassine Azzout
+ * @author Yassine Azzout
+ * @link http://www.92bondstreet.com SwissCode
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+ 
+/**
+ * THE PERFECT PHP CLEAN URL GENERATOR
+ * http://cubiq.org/the-perfect-php-clean-url-generator
+ *
+ * @param $str, $replace, $delimiter
+ *
+ * @return string
+ */
+
+function MacG_toAscii($str, $replace=array(), $delimiter='-') {
+	if( !empty($replace) ) {
+		$str = str_replace((array)$replace, ' ', $str);
+	}
+
+	$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+	$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+	$clean = strtolower(trim($clean, '-'));
+	$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+	$clean = strtolower(trim($clean, '-'));
+	
+	return $clean;
+}
+
+/**
+ * trims text to a space then adds ellipses if desired
+ * @param string $input text to trim
+ * @param int $length in characters to trim to
+ * @param bool $ellipses if ellipses (...) are to be added
+ * @param bool $strip_html if html tags are to be stripped
+ * @return string 
+ */
+function MacG_trim_text($input, $length, $ellipses = true, $strip_html = true) {
+    //strip tags, if desired
+    if ($strip_html) {
+        $input = strip_tags($input);
+    }
+  
+    //no need to trim, already shorter than trim length
+    if (strlen($input) <= $length) {
+        return $input;
+    }
+  
+    //find last space within length
+    $last_space = strrpos(substr($input, 0, $length), ' ');
+    $trimmed_text = substr($input, 0, $last_space);
+  
+    //add ellipses (...)
+    if ($ellipses) {
+        $trimmed_text .= '...';
+    }
+  
+    return $trimmed_text;
+}
+ 
+ 
+?>
